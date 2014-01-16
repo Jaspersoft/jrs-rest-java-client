2532aba814cff6c8db0072c88911784bf1517872
boryskolesnykov <borys.kolesnikov@gmail.com>
2014-01-16 15:05:42 +0200

    Test for multiple sessions was added.

---
+++ src/test/java/com/jaspersoft/jasperserver/jaxrs/client/restservices/demo/MultipleSimultaneousSessionsTest.java
@@ -0,0 +1,166 @@
+package com.jaspersoft.jasperserver.jaxrs.client.restservices.demo;
+
+import com.jaspersoft.jasperserver.dto.reports.ReportExecutionRequest;
+import com.jaspersoft.jasperserver.dto.reports.ReportExecutionStatusEntity;
+import com.jaspersoft.jasperserver.jaxrs.client.JasperserverRestClient;
+import com.jaspersoft.jasperserver.jaxrs.client.RestClientConfiguration;
+import com.jaspersoft.jasperserver.jaxrs.client.builder.OperationResult;
+import com.jaspersoft.jasperserver.jaxrs.client.builder.Session;
+import com.jaspersoft.jasperserver.jaxrs.client.dto.AttachmentDescriptor;
+import com.jaspersoft.jasperserver.jaxrs.client.dto.ExportDescriptor;
+import com.jaspersoft.jasperserver.jaxrs.client.dto.ReportExecutionDescriptor;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.InputStream;
+
+public class MultipleSimultaneousSessionsTest extends Assert {
+
+    private Runnable reportingTask;
+
+    public MultipleSimultaneousSessionsTest(){
+        reportingTask = new Runnable() {
+
+            private Session session;
+            private ReportExecutionDescriptor reportExecutionDescriptor;
+
+            private void init(){
+                RestClientConfiguration configuration = RestClientConfiguration.loadConfiguration("url.properties");
+                JasperserverRestClient client = new JasperserverRestClient(configuration);
+                session = client.authenticate("jasperadmin", "jasperadmin");
+            }
+
+            public void createNewReportRequest(){
+
+                ReportExecutionRequest request = new ReportExecutionRequest();
+                request.setReportUnitUri("/reports/samples/StandardChartsReport");
+                request
+                        .setAsync(true)
+                        .setOutputFormat("html");
+
+                OperationResult<ReportExecutionDescriptor> operationResult =
+                        session
+                                .reportingService()
+                                .newReportRequest(request);
+
+                reportExecutionDescriptor = operationResult.getEntity();
+
+                assertNotEquals(reportExecutionDescriptor, null);
+            }
+
+            public void getReportExecutionStatus(){
+                OperationResult<ReportExecutionStatusEntity> operationResult =
+                        session
+                                .reportingService()
+                                .reportRequest(reportExecutionDescriptor.getRequestId())
+                                .status();
+
+                ReportExecutionStatusEntity statusEntity = operationResult.getEntity();
+                assertNotEquals(statusEntity, null);
+            }
+
+            public void getReportExecutionDetails(){
+                OperationResult<ReportExecutionDescriptor> operationResult =
+                        session
+                                .reportingService()
+                                .reportRequest(reportExecutionDescriptor.getRequestId())
+                                .executionDetails();
+
+                ReportExecutionDescriptor descriptor = operationResult.getEntity();
+                assertNotEquals(descriptor, null);
+            }
+
+            public void getExportOutputResource(){
+                OperationResult<InputStream> operationResult =
+                        session
+                                .reportingService()
+                                .reportRequest(reportExecutionDescriptor.getRequestId())
+                                .export("html")
+                                .outputResource();
+
+                InputStream file = operationResult.getEntity();
+                assertNotEquals(file, null);
+            }
+
+            public void getExportAttachment() throws InterruptedException {
+                OperationResult<ReportExecutionDescriptor> operationResult =
+                        session
+                                .reportingService()
+                                .reportRequest(reportExecutionDescriptor.getRequestId())
+                                .executionDetails();
+
+                ReportExecutionDescriptor descriptor = operationResult.getEntity();
+
+                OperationResult<ReportExecutionStatusEntity> operationResultStatus =
+                        session
+                                .reportingService()
+                                .reportRequest(reportExecutionDescriptor.getRequestId())
+                                .status();
+
+                ReportExecutionStatusEntity statusEntity = operationResultStatus.getEntity();
+
+                while (true){
+
+                    if (statusEntity.getValue().equals("ready")){
+                        ExportDescriptor exportDescriptor = descriptor.getExports().get(0);
+                        AttachmentDescriptor attachmentDescriptor = exportDescriptor.getAttachments().get(0);
+                        String contentType = attachmentDescriptor.getContentType();
+                        String fileName = attachmentDescriptor.getFileName();
+
+                        OperationResult<InputStream> operationResult1 =
+                                session
+                                        .reportingService()
+                                        .reportRequest(reportExecutionDescriptor.getRequestId())
+                                        .export(exportDescriptor.getId())
+                                        .attachment(fileName);
+
+                        InputStream file = operationResult1.getEntity();
+                        assertNotEquals(file, null);
+                        break;
+                    }
+                    else {
+                        Thread.sleep(500);
+                    }
+                }
+
+            }
+
+            public void getReportExportStatus(){
+                OperationResult<ReportExecutionStatusEntity> operationResult =
+                        session
+                                .reportingService()
+                                .reportRequest(reportExecutionDescriptor.getRequestId())
+                                .export("html")
+                                .status();
+
+                ReportExecutionStatusEntity statusEntity = operationResult.getEntity();
+                assertNotEquals(statusEntity, null);
+            }
+
+            @Override
+            public void run() {
+                try {
+                    init();
+                    createNewReportRequest();
+                    getReportExecutionStatus();
+                    getReportExecutionDetails();
+                    getExportOutputResource();
+                    getExportAttachment();
+                    getReportExportStatus();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+    }
+
+    @Test
+    public void testRunThreeSessions() throws InterruptedException {
+        for (int i = 0; i < 3; i++){
+            Thread thread = new Thread(reportingTask);
+            thread.start();
+            thread.join();
+        }
+
+    }
+}
