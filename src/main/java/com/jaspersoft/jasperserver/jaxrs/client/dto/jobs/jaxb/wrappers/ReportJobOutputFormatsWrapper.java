bd132f642facd5fbb1ae44a4dd1fd4aaee2f3135
boryskolesnykov <borys.kolesnikov@gmail.com>
2014-02-28 18:51:09 +0200

    Updated copyrights

--- src/main/java/com/jaspersoft/jasperserver/jaxrs/client/dto/jobs/jaxb/wrappers/ReportJobOutputFormatsWrapper.java
+++ src/main/java/com/jaspersoft/jasperserver/jaxrs/client/dto/jobs/jaxb/wrappers/ReportJobOutputFormatsWrapper.java
@@ -1,25 +1,22 @@
 /*
- * JasperReports - Free Java Reporting Library.
- * Copyright (C) 2001 - 2009 Jaspersoft Corporation. All rights reserved.
+ * Copyright (C) 2005 - 2014 Jaspersoft Corporation. All rights  reserved.
  * http://www.jaspersoft.com.
  *
- * Unless you have purchased a commercial license agreement from Jaspersoft,
- * the following license terms apply:
+ * Unless you have purchased  a commercial license agreement from Jaspersoft,
+ * the following license terms  apply:
  *
- * This program is part of JasperReports.
+ * This program is free software: you can redistribute it and/or  modify
+ * it under the terms of the GNU Affero General Public License  as
+ * published by the Free Software Foundation, either version 3 of  the
+ * License, or (at your option) any later version.
  *
- * JasperReports is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * JasperReports is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Lesser General Public License for more details.
+ * GNU Affero  General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public License
- * along with JasperReports. If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Affero General Public  License
+ * along with this program.&nbsp; If not, see <http://www.gnu.org/licenses/>.
  */
 package com.jaspersoft.jasperserver.jaxrs.client.dto.jobs.jaxb.wrappers;
 
