437be25e160d338047b6673a3b21563e268e4e29
Krasnyanskiy <akrasnyanskiy@ukr.net>
2014-09-20 22:57:29 +0300

    Bumped version to 5.5.0.5-ALPHA after bugfix.

--- src/main/java/com/jaspersoft/jasperserver/jaxrs/client/apiadapters/resources/BatchResourcesAdapter.java
+++ src/main/java/com/jaspersoft/jasperserver/jaxrs/client/apiadapters/resources/BatchResourcesAdapter.java
@@ -18,7 +18,6 @@
  * You should have received a copy of the GNU Affero General Public  License
  * along with this program.&nbsp; If not, see <http://www.gnu.org/licenses/>.
  */
-
 package com.jaspersoft.jasperserver.jaxrs.client.apiadapters.resources;
 
 import com.jaspersoft.jasperserver.dto.resources.ClientResourceListWrapper;
