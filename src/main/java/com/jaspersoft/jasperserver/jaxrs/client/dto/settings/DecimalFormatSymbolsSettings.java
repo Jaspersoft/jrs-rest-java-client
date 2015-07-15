faee4ef38d3c0417ad59defafd5bf9c2a76eeb21
TanyaEf <efimenkot@gmial.com>
2015-07-15 14:34:01 +0300

    Added DecimalFormatSymbolsSettings  DTO

---
+++ src/main/java/com/jaspersoft/jasperserver/jaxrs/client/dto/settings/DecimalFormatSymbolsSettings.java
@@ -0,0 +1,142 @@
+package com.jaspersoft.jasperserver.jaxrs.client.dto.settings;
+
+/**
+ * Created by tetiana.iefimenko on 7/15/2015.
+ */
+public class DecimalFormatSymbolsSettings {
+    private String zeroDigit;
+    private  String groupingSeparator;
+    private  String decimalSeparator;
+    private  String perMill;
+    private  String percent;
+    private  String digit;
+    private  String patternSeparator;
+    private  String infinity;
+    private  String minusSign;
+    private  String currencySymbol;
+    private  String currency;
+    private  String exponentSeparator;
+    private  String internationalCurrencySymbol;
+    private  String monetaryDecimalSeparator;
+    private  String naN;
+
+    public String getZeroDigit() {
+        return zeroDigit;
+    }
+
+    public void setZeroDigit(String zeroDigit) {
+        this.zeroDigit = zeroDigit;
+    }
+
+    public String getGroupingSeparator() {
+        return groupingSeparator;
+    }
+
+    public void setGroupingSeparator(String groupingSeparator) {
+        this.groupingSeparator = groupingSeparator;
+    }
+
+    public String getDecimalSeparator() {
+        return decimalSeparator;
+    }
+
+    public void setDecimalSeparator(String decimalSeparator) {
+        this.decimalSeparator = decimalSeparator;
+    }
+
+    public String getPerMill() {
+        return perMill;
+    }
+
+    public void setPerMill(String perMill) {
+        this.perMill = perMill;
+    }
+
+    public String getPercent() {
+        return percent;
+    }
+
+    public void setPercent(String percent) {
+        this.percent = percent;
+    }
+
+    public String getDigit() {
+        return digit;
+    }
+
+    public void setDigit(String digit) {
+        this.digit = digit;
+    }
+
+    public String getPatternSeparator() {
+        return patternSeparator;
+    }
+
+    public void setPatternSeparator(String patternSeparator) {
+        this.patternSeparator = patternSeparator;
+    }
+
+    public String getInfinity() {
+        return infinity;
+    }
+
+    public void setInfinity(String infinity) {
+        this.infinity = infinity;
+    }
+
+    public String getMinusSign() {
+        return minusSign;
+    }
+
+    public void setMinusSign(String minusSign) {
+        this.minusSign = minusSign;
+    }
+
+    public String getCurrencySymbol() {
+        return currencySymbol;
+    }
+
+    public void setCurrencySymbol(String currencySymbol) {
+        this.currencySymbol = currencySymbol;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public String getExponentSeparator() {
+        return exponentSeparator;
+    }
+
+    public void setExponentSeparator(String exponentSeparator) {
+        this.exponentSeparator = exponentSeparator;
+    }
+
+    public String getInternationalCurrencySymbol() {
+        return internationalCurrencySymbol;
+    }
+
+    public void setInternationalCurrencySymbol(String internationalCurrencySymbol) {
+        this.internationalCurrencySymbol = internationalCurrencySymbol;
+    }
+
+    public String getMonetaryDecimalSeparator() {
+        return monetaryDecimalSeparator;
+    }
+
+    public void setMonetaryDecimalSeparator(String monetaryDecimalSeparator) {
+        this.monetaryDecimalSeparator = monetaryDecimalSeparator;
+    }
+
+    public String getNaN() {
+        return naN;
+    }
+
+    public void setNaN(String naN) {
+        this.naN = naN;
+    }
+}
