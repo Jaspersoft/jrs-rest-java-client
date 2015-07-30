4cbaac223ac27c418c0244246c57a1c878f65053
serhii.blazhyievskyi <serhii.blazhyievskyi@globallogic.com>
2015-07-30 19:04:55 +0300

    To get VM withnew Jasper Reports Server you have to install VirtualBox and Vagrant, next place Vagrant file in your directory for VM and run 'vagrant up --provision'. To stop VM run 'vagrant halt'. To access web interface of Jasper Reports Server go to 'localhost:8090/jasperserver'. This install scripts will download Ubuntu 14.04, install there JRS and run it

---
+++ Vagrant/bootstrap.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+sudo apt-get update
+
+yes | sudo apt-get install python-pip python-dev build-essential python-setuptools
+sudo pip install --upgrade pip 
+sudo pip install --upgrade virtualenv 
+sudo pip install paramiko PyYAML Jinja2 httplib2 six
+
+if [ $(dpkg-query -W -f='${Status}' ansible 2>/dev/null | grep -c "ok installed") -eq 0 ];
+then
+    echo "Add APT repositories"
+    export DEBIAN_FRONTEND=noninteractive
+    apt-get install -qq software-properties-common &> /dev/null || exit 1
+    apt-add-repository ppa:ansible/ansible &> /dev/null || exit 1
+
+    apt-get update -qq
+
+    echo "Installing Ansible"
+    apt-get install -qq ansible &> /dev/null || exit 1
+    echo "Ansible installed"
+fi
+
+echo "Running Ansible"
+bash -c "ansible-playbook /vagrant/playbook.yml --connection=local"
