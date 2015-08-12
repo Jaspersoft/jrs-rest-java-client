92696f5f303aa936781e58b9c39f9b2ec965fa80
serhii.blazhyievskyi <serhii.blazhyievskyi@globallogic.com>
2015-08-12 15:15:11 +0300

    Updated jrsh installation

--- Vagrant/bootstrap.sh
+++ Vagrant/bootstrap.sh
@@ -2,6 +2,15 @@
 
 sudo apt-get update
 
+sudo apt-get install python-software-properties
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+
+sudo echo oracle-java7-installer shared/accepted-oracle-license-v1-1 select true | \
+sudo /usr/bin/debconf-set-selections
+sudo apt-get install oracle-java7-installer -y
+sudo apt-get install oracle-java7-set-default
+
 yes | sudo apt-get install python-pip python-dev build-essential python-setuptools
 sudo pip install --upgrade pip 
 sudo pip install --upgrade virtualenv 
