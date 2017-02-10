b33f6539a574967d7f98c2ce6fc56b6ec266020b
TanyaEf <efimenkot@gmail.com>
2017-02-10 13:39:14 +0200

    Added DomEl expressions collections handling

--- src/main/java/com/jaspersoft/jasperserver/jaxrs/client/apiadapters/datadiscovery/DomElContextManager.java
+++ src/main/java/com/jaspersoft/jasperserver/jaxrs/client/apiadapters/datadiscovery/DomElContextManager.java
@@ -1,9 +1,11 @@
 package com.jaspersoft.jasperserver.jaxrs.client.apiadapters.datadiscovery;
 
+import com.jaspersoft.jasperserver.dto.domain.DomElExpressionCollectionContext;
 import com.jaspersoft.jasperserver.dto.domain.DomElExpressionContext;
 import com.jaspersoft.jasperserver.jaxrs.client.apiadapters.AbstractAdapter;
 import com.jaspersoft.jasperserver.jaxrs.client.apiadapters.context.ContextService;
 import com.jaspersoft.jasperserver.jaxrs.client.core.SessionStorage;
+import com.jaspersoft.jasperserver.jaxrs.client.core.enums.ContextMediaTypes;
 import com.jaspersoft.jasperserver.jaxrs.client.core.operationresult.OperationResult;
 
 /**
@@ -20,9 +22,16 @@ public class DomElContextManager extends AbstractAdapter {
         super(sessionStorage);
     }
 
-    public OperationResult<DomElExpressionContext> create(DomElExpressionContext expression) {
+    public OperationResult<DomElExpressionContext> create(DomElExpressionContext expressionContext) {
         return new ContextService(sessionStorage).
-                context(DomElExpressionContext.class, "application/contexts.domElExpressionContext+json").
-                create(expression);
+                context(DomElExpressionContext.class, ContextMediaTypes.DOM_EL_CONTEXT_JSON).
+                create(expressionContext);
+    }
+
+    public OperationResult<DomElExpressionCollectionContext> create(DomElExpressionCollectionContext expressionCollectionContext) {
+        return new ContextService(sessionStorage).
+                context(DomElExpressionCollectionContext.class,
+                        ContextMediaTypes.DOM_EL_COLLECTION_CONTEXT_JSON).
+                create(expressionCollectionContext);
     }
 }
