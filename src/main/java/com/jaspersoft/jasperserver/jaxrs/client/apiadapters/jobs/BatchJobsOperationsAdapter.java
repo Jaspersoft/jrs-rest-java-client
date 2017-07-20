dde11542a0154f37646d3e1ace508a0ca219946e
TanyaEf <efimenkot@gmail.com>
2017-07-20 18:05:52 +0300

    implemented delete request for multiply jobs, Updated JobsService(switched to server DTO)
    (cherry picked from commit 5fc3dcb9bdeb56588fa524823972ecae04f23343)

--- src/main/java/com/jaspersoft/jasperserver/jaxrs/client/apiadapters/jobs/BatchJobsOperationsAdapter.java
+++ src/main/java/com/jaspersoft/jasperserver/jaxrs/client/apiadapters/jobs/BatchJobsOperationsAdapter.java
@@ -20,6 +20,10 @@
  */
 package com.jaspersoft.jasperserver.jaxrs.client.apiadapters.jobs;
 
+import com.jaspersoft.jasperserver.dto.job.ClientReportJob;
+import com.jaspersoft.jasperserver.dto.job.model.ClientReportJobModel;
+import com.jaspersoft.jasperserver.dto.job.wrappers.ClientJobIdListWrapper;
+import com.jaspersoft.jasperserver.dto.job.wrappers.ClientJobSummariesListWrapper;
 import com.jaspersoft.jasperserver.jaxrs.client.apiadapters.AbstractAdapter;
 import com.jaspersoft.jasperserver.jaxrs.client.core.Callback;
 import com.jaspersoft.jasperserver.jaxrs.client.core.JerseyRequest;
@@ -72,17 +76,31 @@ public class BatchJobsOperationsAdapter extends AbstractAdapter {
         return this;
     }
 
-    public OperationResult<JobSummaryListWrapper> search() {
-        return search(null);
+    public BatchJobsOperationsAdapter parameters(JobsParameter parameter, String... values) {
+        for (String value : values) {
+            params.add(parameter.getName(), value);
+        }
+        return this;
     }
 
-    public OperationResult<JobSummaryListWrapper> search(Job searchCriteria) {
-        JerseyRequest<JobSummaryListWrapper> request = prepareSearchRequest(searchCriteria);
+    public BatchJobsOperationsAdapter parameters(JobsParameter parameter, int... values) {
+        for (int value : values) {
+            params.add(parameter.getName(), String.valueOf(value));
+        }
+        return this;
+    }
+
+    public OperationResult<ClientJobSummariesListWrapper> searchJobs() {
+        return searchJobs(null);
+    }
+
+    public OperationResult<ClientJobSummariesListWrapper> searchJobs(ClientReportJob searchCriteria) {
+        JerseyRequest<ClientJobSummariesListWrapper> request = prepareSearchRequest(searchCriteria);
         return request.get();
     }
 
-    public <R> RequestExecution asyncSearch(final Job searchCriteria, final Callback<OperationResult<JobSummaryListWrapper>, R> callback) {
-        final JerseyRequest<JobSummaryListWrapper> request = prepareSearchRequest(searchCriteria);
+    public <R> RequestExecution asyncSearchJobs(final ClientReportJob searchCriteria, final Callback<OperationResult<ClientJobSummariesListWrapper>, R> callback) {
+        final JerseyRequest<ClientJobSummariesListWrapper> request = prepareSearchRequest(searchCriteria);
         RequestExecution task = new RequestExecution(new Runnable() {
             @Override
             public void run() {
@@ -93,8 +111,8 @@ public class BatchJobsOperationsAdapter extends AbstractAdapter {
         return task;
     }
 
-    private JerseyRequest<JobSummaryListWrapper> prepareSearchRequest(Job searchCriteria) {
-        JerseyRequest<JobSummaryListWrapper> request = buildRequest(sessionStorage, JobSummaryListWrapper.class, new String[]{SERVICE_URI});
+    private JerseyRequest<ClientJobSummariesListWrapper> prepareSearchRequest(ClientReportJob searchCriteria) {
+        JerseyRequest<ClientJobSummariesListWrapper> request = buildRequest(sessionStorage, ClientJobSummariesListWrapper.class, new String[]{SERVICE_URI});
         request.addParams(params);
         if (searchCriteria != null) {
             String criteriaJson = buildJson(searchCriteria);
@@ -120,10 +138,10 @@ public class BatchJobsOperationsAdapter extends AbstractAdapter {
         }
     }
 
-    private String buildXml(ReportJobModel reportJobModel) {
+    private String buildXml(ClientReportJobModel reportJobModel) {
         try {
             StringWriter writer = new StringWriter();
-            JAXBContext jaxbContext = JAXBContext.newInstance(ReportJobModel.class);
+            JAXBContext jaxbContext = JAXBContext.newInstance(ClientReportJobModel.class);
             Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
             jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
             jaxbMarshaller.marshal(reportJobModel, writer);
@@ -135,15 +153,15 @@ public class BatchJobsOperationsAdapter extends AbstractAdapter {
     }
 
     @Deprecated
-    public OperationResult<JobIdListWrapper> updateWithProcessedParameters(ReportJobModel jobModel) {
+    public OperationResult<ClientJobIdListWrapper> updateWithProcessedParameters(ClientReportJobModel jobModel) {
         throw new UnsupportedOperationException("Operation is not supported.");
     }
 
     /**
      * Updates all jobs which ids were specified. Updates only set fields, other fields are ignored.
      */
-    public OperationResult<JobIdListWrapper> update(ReportJobModel jobModel) {
-        JerseyRequest<JobIdListWrapper> request = buildRequest(sessionStorage, JobIdListWrapper.class, new String[]{SERVICE_URI});
+    public OperationResult<ClientJobIdListWrapper> update(ClientReportJobModel jobModel) {
+        JerseyRequest<ClientJobIdListWrapper> request = buildRequest(sessionStorage, ClientJobIdListWrapper.class, new String[]{SERVICE_URI});
         request.addParams(params);
         String content;
 
@@ -155,8 +173,8 @@ public class BatchJobsOperationsAdapter extends AbstractAdapter {
         return request.post(content);
     }
 
-    public <R> RequestExecution asyncUpdate(final ReportJobModel jobModel, final Callback<OperationResult<JobIdListWrapper>, R> callback) {
-        final JerseyRequest<JobIdListWrapper> request = buildRequest(sessionStorage, JobIdListWrapper.class, new String[]{SERVICE_URI});
+    public <R> RequestExecution asyncUpdate(final ClientReportJobModel jobModel, final Callback<OperationResult<ClientJobIdListWrapper>, R> callback) {
+        final JerseyRequest<ClientJobIdListWrapper> request = buildRequest(sessionStorage, ClientJobIdListWrapper.class, new String[]{SERVICE_URI});
         request.addParams(params);
         final String jobJson = buildJson(jobModel);
         RequestExecution task = new RequestExecution(new Runnable() {
@@ -181,11 +199,160 @@ public class BatchJobsOperationsAdapter extends AbstractAdapter {
         return ids;
     }
 
+    public OperationResult<ClientJobIdListWrapper> pauseJobs() {
+        ClientJobIdListWrapper jobIdListWrapper = new ClientJobIdListWrapper(getIds());
+        return buildRequest(sessionStorage, ClientJobIdListWrapper.class, new String[]{SERVICE_URI, PAUSE}).post(jobIdListWrapper);
+    }
+
+    public <R> RequestExecution asyncPauseJobs(final Callback<OperationResult<ClientJobIdListWrapper>, R> callback) {
+        final ClientJobIdListWrapper jobIdListWrapper = new ClientJobIdListWrapper(getIds());
+        final JerseyRequest<ClientJobIdListWrapper> request = buildRequest(sessionStorage, ClientJobIdListWrapper.class, new String[]{SERVICE_URI, PAUSE});
+        RequestExecution task = new RequestExecution(new Runnable() {
+            @Override
+            public void run() {
+                callback.execute(request.post(jobIdListWrapper));
+            }
+        });
+        ThreadPoolUtil.runAsynchronously(task);
+        return task;
+    }
+
+    public OperationResult<ClientJobIdListWrapper> resumeJobs() {
+        ClientJobIdListWrapper jobIdListWrapper = new ClientJobIdListWrapper(getIds());
+        return buildRequest(sessionStorage, ClientJobIdListWrapper.class, new String[]{SERVICE_URI, RESUME}).post(jobIdListWrapper);
+    }
+
+    public <R> RequestExecution asyncResumeJobs(final Callback<OperationResult<ClientJobIdListWrapper>, R> callback) {
+        final ClientJobIdListWrapper jobIdListWrapper = new ClientJobIdListWrapper(getIds());
+        final JerseyRequest<ClientJobIdListWrapper> request = buildRequest(sessionStorage, ClientJobIdListWrapper.class, new String[]{SERVICE_URI, RESUME});
+        RequestExecution task = new RequestExecution(new Runnable() {
+            @Override
+            public void run() {
+                callback.execute(request.post(jobIdListWrapper));
+            }
+        });
+        ThreadPoolUtil.runAsynchronously(task);
+        return task;
+    }
+
+    public OperationResult<ClientJobIdListWrapper> restartJobs() {
+        ClientJobIdListWrapper jobIdListWrapper = new ClientJobIdListWrapper(getIds());
+        return buildRequest(sessionStorage, ClientJobIdListWrapper.class, new String[]{SERVICE_URI, RESTART}).post(jobIdListWrapper);
+    }
+
+    public <R> RequestExecution asyncRestartJobs(final Callback<OperationResult<ClientJobIdListWrapper>, R> callback) {
+        final ClientJobIdListWrapper jobIdListWrapper = new ClientJobIdListWrapper(getIds());
+        final JerseyRequest<ClientJobIdListWrapper> request = buildRequest(sessionStorage, ClientJobIdListWrapper.class, new String[]{SERVICE_URI, RESTART});
+        RequestExecution task = new RequestExecution(new Runnable() {
+            @Override
+            public void run() {
+                callback.execute(request.post(jobIdListWrapper));
+            }
+        });
+        ThreadPoolUtil.runAsynchronously(task);
+        return task;
+    }
+
+    /**
+     * @deprecated Replaced by {@link BatchJobsOperationsAdapter#searchJobs()}.
+     */
+    public OperationResult<JobSummaryListWrapper> search() {
+        return search(null);
+    }
+    /**
+     * @deprecated Replaced by {@link BatchJobsOperationsAdapter#searchJobs(com.jaspersoft.jasperserver.dto.job.ClientReportJob)}.
+     */
+    public OperationResult<JobSummaryListWrapper> search(Job searchCriteria) {
+        JerseyRequest<JobSummaryListWrapper> request = prepareSearchRequest(searchCriteria);
+        return request.get();
+    }
+    /**
+     * @deprecated Replaced by {@link BatchJobsOperationsAdapter#asyncSearchJobs(com.jaspersoft.jasperserver.dto.job.ClientReportJob, com.jaspersoft.jasperserver.jaxrs.client.core.Callback)}.
+     */
+    public <R> RequestExecution asyncSearch(final Job searchCriteria, final Callback<OperationResult<JobSummaryListWrapper>, R> callback) {
+        final JerseyRequest<JobSummaryListWrapper> request = prepareSearchRequest(searchCriteria);
+        RequestExecution task = new RequestExecution(new Runnable() {
+            @Override
+            public void run() {
+                callback.execute(request.get());
+            }
+        });
+        ThreadPoolUtil.runAsynchronously(task);
+        return task;
+    }
+    /**
+     * @deprecated Replaced by {@link BatchJobsOperationsAdapter#prepareSearchRequest(com.jaspersoft.jasperserver.dto.job.ClientReportJob)}.
+     */
+    private JerseyRequest<JobSummaryListWrapper> prepareSearchRequest(Job searchCriteria) {
+        JerseyRequest<JobSummaryListWrapper> request = buildRequest(sessionStorage, JobSummaryListWrapper.class, new String[]{SERVICE_URI});
+        request.addParams(params);
+        if (searchCriteria != null) {
+            String criteriaJson = buildJson(searchCriteria);
+            request.addParam("example", URLEncoder.encode(criteriaJson), "UTF-8");
+        }
+        return request;
+    }
+
+    /**
+     * @deprecated Replaced by {@link BatchJobsOperationsAdapter#update(com.jaspersoft.jasperserver.dto.job.model.ClientReportJobModel)}.
+     */
+    public OperationResult<JobIdListWrapper> update(ReportJobModel jobModel) {
+        JerseyRequest<JobIdListWrapper> request = buildRequest(sessionStorage, JobIdListWrapper.class, new String[]{SERVICE_URI});
+        request.addParams(params);
+        String content;
+
+        if (sessionStorage.getConfiguration().getContentMimeType() == MimeType.JSON) {
+            content = buildJson(jobModel);
+        } else {
+            content = buildXml(jobModel);
+        }
+        return request.post(content);
+    }
+
+    /**
+     * @deprecated Replaced by {@link BatchJobsOperationsAdapter#buildXml(com.jaspersoft.jasperserver.dto.job.model.ClientReportJobModel)}.
+     */
+    private String buildXml(ReportJobModel reportJobModel) {
+        try {
+            StringWriter writer = new StringWriter();
+            JAXBContext jaxbContext = JAXBContext.newInstance(ReportJobModel.class);
+            Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
+            jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+            jaxbMarshaller.marshal(reportJobModel, writer);
+            return writer.toString();
+        } catch (JAXBException e) {
+            log.warn("Can't marshal report job model.");
+            throw new RuntimeException("Failed inFolder build report job model xml.", e);
+        }
+    }
+
+    /**
+     * @deprecated Replaced by {@link BatchJobsOperationsAdapter#asyncUpdate(com.jaspersoft.jasperserver.dto.job.model.ClientReportJobModel, com.jaspersoft.jasperserver.jaxrs.client.core.Callback)}.
+     */
+    public <R> RequestExecution asyncUpdate(final ReportJobModel jobModel, final Callback<OperationResult<JobIdListWrapper>, R> callback) {
+        final JerseyRequest<JobIdListWrapper> request = buildRequest(sessionStorage, JobIdListWrapper.class, new String[]{SERVICE_URI});
+        request.addParams(params);
+        final String jobJson = buildJson(jobModel);
+        RequestExecution task = new RequestExecution(new Runnable() {
+            @Override
+            public void run() {
+                callback.execute(request.post(jobJson));
+            }
+        });
+        ThreadPoolUtil.runAsynchronously(task);
+        return task;
+    }
+    /**
+     * @deprecated Replaced by {@link com.jaspersoft.jasperserver.jaxrs.client.apiadapters.jobs.BatchJobsOperationsAdapter#pauseJobs()}.
+     */
     public OperationResult<JobIdListWrapper> pause() {
         JobIdListWrapper jobIdListWrapper = new JobIdListWrapper(getIds());
         return buildRequest(sessionStorage, JobIdListWrapper.class, new String[]{SERVICE_URI, PAUSE}).post(jobIdListWrapper);
     }
 
+    /**
+     * @deprecated Replaced by {@link BatchJobsOperationsAdapter#asyncPauseJobs(com.jaspersoft.jasperserver.jaxrs.client.core.Callback)}.
+     */
     public <R> RequestExecution asyncPause(final Callback<OperationResult<JobIdListWrapper>, R> callback) {
         final JobIdListWrapper jobIdListWrapper = new JobIdListWrapper(getIds());
         final JerseyRequest<JobIdListWrapper> request = buildRequest(sessionStorage, JobIdListWrapper.class, new String[]{SERVICE_URI, PAUSE});
@@ -199,11 +366,17 @@ public class BatchJobsOperationsAdapter extends AbstractAdapter {
         return task;
     }
 
+    /**
+     * @deprecated Replaced by {@link com.jaspersoft.jasperserver.jaxrs.client.apiadapters.jobs.BatchJobsOperationsAdapter#resumeJobs()}.
+     */
     public OperationResult<JobIdListWrapper> resume() {
         JobIdListWrapper jobIdListWrapper = new JobIdListWrapper(getIds());
         return buildRequest(sessionStorage, JobIdListWrapper.class, new String[]{SERVICE_URI, RESUME}).post(jobIdListWrapper);
     }
 
+    /**
+     * @deprecated Replaced by {@link BatchJobsOperationsAdapter#asyncResumeJobs(com.jaspersoft.jasperserver.jaxrs.client.core.Callback)}.
+     */
     public <R> RequestExecution asyncResume(final Callback<OperationResult<JobIdListWrapper>, R> callback) {
         final JobIdListWrapper jobIdListWrapper = new JobIdListWrapper(getIds());
         final JerseyRequest<JobIdListWrapper> request = buildRequest(sessionStorage, JobIdListWrapper.class, new String[]{SERVICE_URI, RESUME});
@@ -217,11 +390,16 @@ public class BatchJobsOperationsAdapter extends AbstractAdapter {
         return task;
     }
 
+    /**
+     * @deprecated Replaced by {@link com.jaspersoft.jasperserver.jaxrs.client.apiadapters.jobs.BatchJobsOperationsAdapter#restartJobs()}.
+     */
     public OperationResult<JobIdListWrapper> restart() {
         JobIdListWrapper jobIdListWrapper = new JobIdListWrapper(getIds());
         return buildRequest(sessionStorage, JobIdListWrapper.class, new String[]{SERVICE_URI, RESTART}).post(jobIdListWrapper);
     }
-
+    /**
+     * @deprecated Replaced by {@link BatchJobsOperationsAdapter#asyncRestartJobs(com.jaspersoft.jasperserver.jaxrs.client.core.Callback)}.
+     */
     public <R> RequestExecution asyncRestart(final Callback<OperationResult<JobIdListWrapper>, R> callback) {
         final JobIdListWrapper jobIdListWrapper = new JobIdListWrapper(getIds());
         final JerseyRequest<JobIdListWrapper> request = buildRequest(sessionStorage, JobIdListWrapper.class, new String[]{SERVICE_URI, RESTART});
@@ -234,4 +412,11 @@ public class BatchJobsOperationsAdapter extends AbstractAdapter {
         ThreadPoolUtil.runAsynchronously(task);
         return task;
     }
+
+    public OperationResult<ClientJobIdListWrapper> delete() {
+        JerseyRequest<ClientJobIdListWrapper> jerseyRequest = buildRequest(sessionStorage, ClientJobIdListWrapper.class, new String[]{SERVICE_URI});
+        jerseyRequest.addParams(params);
+        return jerseyRequest.delete();
+    }
+
 }
