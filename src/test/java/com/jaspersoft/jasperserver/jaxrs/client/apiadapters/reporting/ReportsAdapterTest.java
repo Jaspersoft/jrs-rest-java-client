b78d3af2d75d19c4a2a36f1d237832327c535af9
TanyaEf <efimenkot@gmail.com>
2017-09-11 18:40:47 +0300

    Implemented ReportOptionsService

--- src/test/java/com/jaspersoft/jasperserver/jaxrs/client/apiadapters/reporting/ReportsAdapterTest.java
+++ src/test/java/com/jaspersoft/jasperserver/jaxrs/client/apiadapters/reporting/ReportsAdapterTest.java
@@ -1,8 +1,10 @@
 package com.jaspersoft.jasperserver.jaxrs.client.apiadapters.reporting;
 
+import com.jaspersoft.jasperserver.jaxrs.client.apiadapters.reporting.peroprtoptions.ReportOptionsAdapter;
 import com.jaspersoft.jasperserver.jaxrs.client.apiadapters.reporting.reportparameters.ReorderingReportParametersAdapter;
 import com.jaspersoft.jasperserver.jaxrs.client.apiadapters.reporting.reportparameters.ReportParametersAdapter;
 import com.jaspersoft.jasperserver.jaxrs.client.core.SessionStorage;
+import javax.ws.rs.core.MultivaluedHashMap;
 import org.mockito.Mock;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
@@ -24,11 +26,16 @@ import static org.testng.Assert.assertSame;
 @PrepareForTest({ReorderingReportParametersAdapter.class, ReportParametersAdapter.class, /*RunReportAdapter.class,*/ ReportsAdapter.class})
 public class ReportsAdapterTest extends PowerMockTestCase {
 
+    public static final String REPORT_UNIT_URI = "reportUnitUri";
+    public static final String OPTIONS_ID = "optionsId";
     @Mock
     private SessionStorage sessionStorageMock;
 
     @Mock
-    private ReorderingReportParametersAdapter adapterMock;
+    private ReorderingReportParametersAdapter reportParametersAdapterMock;
+
+    @Mock
+    private ReportOptionsAdapter reportOptionsAdapterMock;
 
     @Mock
     private RunReportAdapter reportAdapterMock;
@@ -45,25 +52,65 @@ public class ReportsAdapterTest extends PowerMockTestCase {
     public void should_pass_session_to_parent_class() {
 
         /* When */
-        ReportsAdapter adapterSpy = new ReportsAdapter(sessionStorageMock, "reportUnitUri");
+        ReportsAdapter adapterSpy = new ReportsAdapter(sessionStorageMock, REPORT_UNIT_URI);
 
         /* Then */
         assertSame(adapterSpy.getSessionStorage(), sessionStorageMock);
-        String reportUnitUri = Whitebox.getInternalState(adapterSpy, "reportUnitUri");
-        assertEquals(reportUnitUri, "reportUnitUri");
+        String reportUnitUri = Whitebox.getInternalState(adapterSpy, REPORT_UNIT_URI);
+        assertEquals(reportUnitUri, REPORT_UNIT_URI);
+    }
+
+    @Test
+    public void should_return_proper_ReportOptionsAdapter_object() throws Exception {
+
+        /* When */
+        PowerMockito.whenNew(ReportOptionsAdapter .class).withArguments(sessionStorageMock, REPORT_UNIT_URI).thenReturn(reportOptionsAdapterMock);
+
+        ReportsAdapter adapterSpy = new ReportsAdapter(sessionStorageMock, REPORT_UNIT_URI);
+        ReportOptionsAdapter retrieved = adapterSpy.reportOptions();
+
+        assertSame(retrieved, reportOptionsAdapterMock);
+        verifyNew(ReportOptionsAdapter.class, times(1)).withArguments(sessionStorageMock, REPORT_UNIT_URI);
+    }
+
+
+    @Test
+    public void should_return_proper_ReportOptionsAdapter_object_by_optionsId() throws Exception {
+
+        /* When */
+        PowerMockito.whenNew(ReportOptionsAdapter .class).withArguments(sessionStorageMock, REPORT_UNIT_URI, OPTIONS_ID).thenReturn(reportOptionsAdapterMock);
+
+        ReportsAdapter adapterSpy = new ReportsAdapter(sessionStorageMock, REPORT_UNIT_URI);
+        ReportOptionsAdapter retrieved = adapterSpy.reportOptions(OPTIONS_ID);
+
+        assertSame(retrieved, reportOptionsAdapterMock);
+        verifyNew(ReportOptionsAdapter.class, times(1)).withArguments(sessionStorageMock, REPORT_UNIT_URI, OPTIONS_ID);
+    }
+
+    @Test
+    public void should_return_proper_ReportOptionsAdapter_object_by_options_map() throws Exception {
+        MultivaluedHashMap<String, String> map = new MultivaluedHashMap<>();
+        /* When */
+        PowerMockito.whenNew(ReportOptionsAdapter .class).withArguments(sessionStorageMock, REPORT_UNIT_URI, map).thenReturn(reportOptionsAdapterMock);
+
+        ReportsAdapter adapterSpy = new ReportsAdapter(sessionStorageMock, REPORT_UNIT_URI);
+        ReportOptionsAdapter retrieved = adapterSpy.reportOptions(map);
+
+        assertSame(retrieved, reportOptionsAdapterMock);
+        verifyNew(ReportOptionsAdapter.class, times(1)).withArguments(sessionStorageMock, REPORT_UNIT_URI, map);
     }
 
     @Test
     public void should_return_proper_ReorderingReportParametersAdapter_object() throws Exception {
 
         /* When */
-        PowerMockito.whenNew(ReorderingReportParametersAdapter.class).withArguments(sessionStorageMock, "reportUnitUri").thenReturn(adapterMock);
+        PowerMockito.whenNew(ReorderingReportParametersAdapter.class).withArguments(sessionStorageMock, REPORT_UNIT_URI).thenReturn(reportParametersAdapterMock);
 
-        ReportsAdapter adapterSpy = new ReportsAdapter(sessionStorageMock, "reportUnitUri");
+        ReportsAdapter adapterSpy = new ReportsAdapter(sessionStorageMock, REPORT_UNIT_URI);
         ReorderingReportParametersAdapter retrieved = adapterSpy.reportParameters();
 
-        assertSame(retrieved, adapterMock);
-        verifyNew(ReorderingReportParametersAdapter.class, times(1)).withArguments(sessionStorageMock, "reportUnitUri");
+        assertSame(retrieved, reportParametersAdapterMock);
+        verifyNew(ReorderingReportParametersAdapter.class, times(1)).withArguments(sessionStorageMock, REPORT_UNIT_URI);
     }
 
     @Test
@@ -71,43 +118,43 @@ public class ReportsAdapterTest extends PowerMockTestCase {
 
         /* Given */
         ReportParametersAdapter parametersAdapterMock = mock(ReportParametersAdapter.class);
-        whenNew(ReportParametersAdapter.class).withArguments(sessionStorageMock, "reportUnitUri", "1;2;3;4;5;").thenReturn(parametersAdapterMock);
-        ReportsAdapter adapterSpy = new ReportsAdapter(sessionStorageMock, "reportUnitUri");
+        whenNew(ReportParametersAdapter.class).withArguments(sessionStorageMock, REPORT_UNIT_URI, "1;2;3;4;5;").thenReturn(parametersAdapterMock);
+        ReportsAdapter adapterSpy = new ReportsAdapter(sessionStorageMock, REPORT_UNIT_URI);
 
         /* When */
         ReportParametersAdapter retrieved = adapterSpy.reportParameters("1", "2", "3", "4", "5");
 
         /* Then */
         assertSame(retrieved, parametersAdapterMock);
-        verifyNew(ReportParametersAdapter.class, times(1)).withArguments(sessionStorageMock, "reportUnitUri", "1;2;3;4;5;");
+        verifyNew(ReportParametersAdapter.class, times(1)).withArguments(sessionStorageMock, REPORT_UNIT_URI, "1;2;3;4;5;");
     }
 
     @Test
     public void should_convert_params_into_the_adapter_with_pages() throws Exception {
 
         /* Given */
-        ReportsAdapter adapterSpy = new ReportsAdapter(sessionStorageMock, "reportUnitUri");
+        ReportsAdapter adapterSpy = new ReportsAdapter(sessionStorageMock, REPORT_UNIT_URI);
 
         /* When */
         RunReportAdapter retrieved = adapterSpy.prepareForRun(ReportOutputFormat.PDF, 1, 2, 3);
 
         /* Then */
         assertSame(retrieved.getSessionStorage(), sessionStorageMock);
-        assertEquals(Whitebox.getInternalState(retrieved, "reportUnitUri"), "reportUnitUri");
+        assertEquals(Whitebox.getInternalState(retrieved, REPORT_UNIT_URI), REPORT_UNIT_URI);
         assertEquals(Whitebox.getInternalState(retrieved, "pages"), new String[]{"1", "2", "3"});
     }
     @Test
     public void should_convert_params_into_the_adapter_with_format() throws Exception {
 
         /* Given */
-        ReportsAdapter adapterSpy = new ReportsAdapter(sessionStorageMock, "reportUnitUri");
+        ReportsAdapter adapterSpy = new ReportsAdapter(sessionStorageMock, REPORT_UNIT_URI);
 
         /* When */
         RunReportAdapter retrieved = adapterSpy.prepareForRun(ReportOutputFormat.PDF);
 
         /* Then */
         assertSame(retrieved.getSessionStorage(), sessionStorageMock);
-        assertEquals(Whitebox.getInternalState(retrieved, "reportUnitUri"), "reportUnitUri");
+        assertEquals(Whitebox.getInternalState(retrieved, REPORT_UNIT_URI), REPORT_UNIT_URI);
         assertEquals(Whitebox.getInternalState(retrieved, "format"), new String("pdf"));
     }
 
@@ -115,14 +162,14 @@ public class ReportsAdapterTest extends PowerMockTestCase {
     public void should_convert_params_into_the_adapter_without_pages() throws Exception {
 
         /* Given */
-        ReportsAdapter adapterSpy = new ReportsAdapter(sessionStorageMock, "reportUnitUri");
+        ReportsAdapter adapterSpy = new ReportsAdapter(sessionStorageMock, REPORT_UNIT_URI);
 
         /* When */
         RunReportAdapter retrieved = adapterSpy.prepareForRun(ReportOutputFormat.PDF, new PageRange(1, 10));
 
         /* Then */
         assertSame(retrieved.getSessionStorage(), sessionStorageMock);
-        assertEquals(Whitebox.getInternalState(retrieved, "reportUnitUri"), "reportUnitUri");
+        assertEquals(Whitebox.getInternalState(retrieved, REPORT_UNIT_URI), REPORT_UNIT_URI);
         assertEquals(Whitebox.getInternalState(retrieved, "pages"), new String[]{"1-10"});
 
     }
@@ -131,14 +178,14 @@ public class ReportsAdapterTest extends PowerMockTestCase {
     public void should_convert_params_into_the_adapter_with_pages_and_format_as_string() throws Exception {
 
         /* Given */
-        ReportsAdapter adapterSpy = new ReportsAdapter(sessionStorageMock, "reportUnitUri");
+        ReportsAdapter adapterSpy = new ReportsAdapter(sessionStorageMock, REPORT_UNIT_URI);
 
         /* When */
         RunReportAdapter retrieved = adapterSpy.prepareForRun("PDF", 1, 2, 3);
 
         /* Then */
         assertSame(retrieved.getSessionStorage(), sessionStorageMock);
-        assertEquals(Whitebox.getInternalState(retrieved, "reportUnitUri"), "reportUnitUri");
+        assertEquals(Whitebox.getInternalState(retrieved, REPORT_UNIT_URI), REPORT_UNIT_URI);
         assertEquals(Whitebox.getInternalState(retrieved, "pages"), new String[]{"1", "2", "3"});
         assertEquals(Whitebox.getInternalState(retrieved, "format"), "pdf");
     }
@@ -146,14 +193,14 @@ public class ReportsAdapterTest extends PowerMockTestCase {
     public void should_convert_params_into_the_adapter_with_format_as_string_in_uppercase() throws Exception {
 
         /* Given */
-        ReportsAdapter adapterSpy = new ReportsAdapter(sessionStorageMock, "reportUnitUri");
+        ReportsAdapter adapterSpy = new ReportsAdapter(sessionStorageMock, REPORT_UNIT_URI);
 
         /* When */
         RunReportAdapter retrieved = adapterSpy.prepareForRun("HTML");
 
         /* Then */
         assertSame(retrieved.getSessionStorage(), sessionStorageMock);
-        assertEquals(Whitebox.getInternalState(retrieved, "reportUnitUri"), "reportUnitUri");
+        assertEquals(Whitebox.getInternalState(retrieved, REPORT_UNIT_URI), REPORT_UNIT_URI);
         assertEquals(Whitebox.getInternalState(retrieved, "format"), "html");
 
     }
@@ -161,14 +208,14 @@ public class ReportsAdapterTest extends PowerMockTestCase {
     public void should_convert_params_into_the_adapter_with_format_as_string() throws Exception {
 
         /* Given */
-        ReportsAdapter adapterSpy = new ReportsAdapter(sessionStorageMock, "reportUnitUri");
+        ReportsAdapter adapterSpy = new ReportsAdapter(sessionStorageMock, REPORT_UNIT_URI);
 
         /* When */
         RunReportAdapter retrieved = adapterSpy.prepareForRun("pdf");
 
         /* Then */
         assertSame(retrieved.getSessionStorage(), sessionStorageMock);
-        assertEquals(Whitebox.getInternalState(retrieved, "reportUnitUri"), "reportUnitUri");
+        assertEquals(Whitebox.getInternalState(retrieved, REPORT_UNIT_URI), REPORT_UNIT_URI);
         assertEquals(Whitebox.getInternalState(retrieved, "format"), "pdf");
 
     }
@@ -177,20 +224,20 @@ public class ReportsAdapterTest extends PowerMockTestCase {
     public void should_convert_params_into_the_adapter_without_pages_as_string() throws Exception {
 
         /* Given */
-        ReportsAdapter adapterSpy = new ReportsAdapter(sessionStorageMock, "reportUnitUri");
+        ReportsAdapter adapterSpy = new ReportsAdapter(sessionStorageMock, REPORT_UNIT_URI);
 
         /* When */
         RunReportAdapter retrieved = adapterSpy.prepareForRun("pdf", new PageRange(1, 10));
 
         /* Then */
         assertSame(retrieved.getSessionStorage(), sessionStorageMock);
-        assertEquals(Whitebox.getInternalState(retrieved, "reportUnitUri"), "reportUnitUri");
+        assertEquals(Whitebox.getInternalState(retrieved, REPORT_UNIT_URI), REPORT_UNIT_URI);
         assertEquals(Whitebox.getInternalState(retrieved, "pages"), new String[]{"1-10"});
         assertEquals(Whitebox.getInternalState(retrieved, "format"), "pdf");
     }
 
     @AfterMethod
     public void after() {
-        reset(sessionStorageMock, /*formatMock, */reportAdapterMock, parametersAdapterMock, adapterMock);
+        reset(sessionStorageMock, /*formatMock, */reportAdapterMock, parametersAdapterMock, reportOptionsAdapterMock, reportParametersAdapterMock);
     }
 }
