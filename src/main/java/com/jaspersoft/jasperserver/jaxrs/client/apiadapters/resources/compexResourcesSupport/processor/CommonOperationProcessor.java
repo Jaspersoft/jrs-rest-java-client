896b721473656591065b63a782707d6d02fb1c0b
TanyaEf <efimenkot@gmail.com>
2017-11-09 13:28:30 +0200

    Refactoring in ResourceService

---
+++ src/main/java/com/jaspersoft/jasperserver/jaxrs/client/apiadapters/resources/compexResourcesSupport/processor/CommonOperationProcessor.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2005 - 2014 Jaspersoft Corporation. All rights  reserved.
+ * http://www.jaspersoft.com.
+ *
+ * Unless you have purchased  a commercial license agreement from Jaspersoft,
+ * the following license terms  apply:
+ *
+ * This program is free software: you can redistribute it and/or  modify
+ * it under the terms of the GNU Affero General Public License  as
+ * published by the Free Software Foundation, either version 3 of  the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero  General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public  License
+ * along with this program.&nbsp; If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.jaspersoft.jasperserver.jaxrs.client.apiadapters.resources.compexResourcesSupport.processor;
+
+import com.jaspersoft.jasperserver.jaxrs.client.core.operationresult.OperationResult;
+import javax.ws.rs.core.MediaType;
+import org.glassfish.jersey.media.multipart.FormDataMultiPart;
+
+/**
+ * @author Alexander Krasnyanskiy
+ */
+public interface CommonOperationProcessor<ResourceType> {
+    OperationResult<ResourceType> create(FormDataMultiPart multipart, MediaType mediaType, String path);
+    OperationResult<ResourceType> get(String uri);
+}
