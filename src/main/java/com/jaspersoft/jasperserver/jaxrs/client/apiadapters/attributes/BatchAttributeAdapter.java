ee85de1877b7b28c70bc6faa61b4a8f6d645aa41
Tetyana Matveyeva <tmatveye@tibco.com>
2018-03-26 16:14:02 +0200

    fixed mime type for search attributes in case if there is _embedded parameter

--- src/main/java/com/jaspersoft/jasperserver/jaxrs/client/apiadapters/attributes/BatchAttributeAdapter.java
+++ src/main/java/com/jaspersoft/jasperserver/jaxrs/client/apiadapters/attributes/BatchAttributeAdapter.java
@@ -213,10 +213,11 @@ public class BatchAttributeAdapter extends AbstractAdapter {
                 sessionStorage,
                 HypermediaAttributesListWrapper.class,
                 new String[]{SERVICE_URI}, new DefaultErrorHandler());
+        request.setAccept(MimeTypeUtil.toCorrectAcceptMime(sessionStorage.getConfiguration(), "application/attributes.collection+{mime}"));
         if (includePermissions) {
+            request.setAccept(MimeTypeUtil.toCorrectAcceptMime(sessionStorage.getConfiguration(), "application/attributes.collection.hal+{mime}"));
             request.addParam("_embedded", "permission");
         }
-        request.setAccept(MimeTypeUtil.toCorrectAcceptMime(sessionStorage.getConfiguration(), "application/attributes.collection+{mime}"));
         request.addParams(params);
 
         // if user did not specified holder make it form organizationId and userName variables
